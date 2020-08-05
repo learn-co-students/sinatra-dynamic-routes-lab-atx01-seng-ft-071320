924f2a1f90161e6f79038d50dcd03d260bf8856e
Christorres392 <christorres09@gmail.com>
2020-08-05 12:42:33 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,40 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+  
+  get "/reversename/:name" do
+    @user_name = params[:name]
+    @reverse_name = @user_name.reverse
+    "#{@reverse_name}"
+  end
+
+  get "/square/:number" do
+    square_num = params[:number].to_i * params[:number].to_i
+    "#{square_num}"
+  end
+
+  get "/say/:number/:phrase" do
+    "#{params[:phrase]}" * params[:number].to_i
+  end
+
+  get "/say/:word1/:word2/:word3/:word4/:word5" do
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+  end
+
+  get "/:operation/:number1/:number2" do
+    if params[:operation] == "add"
+      add = params[:number1].to_i + params[:number2].to_i
+      "#{add}"
+    elsif params[:operation] == "subtract"
+    subtract = params[:number1].to_i - params[:number2].to_i
+    "#{subtract}"
+    elsif params[:operation] == "multiply"
+      multiply = params[:number1].to_i * params[:number2].to_i
+      "#{multiply}"
+    elsif params[:operation] == "divide"
+      divide = params[:number1].to_i / params[:number2].to_i
+      "#{divide}"
+    end
+  end
 
 end
